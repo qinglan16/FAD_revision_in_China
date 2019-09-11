f79a80c2236e7995be0161c212945d1abb919e54
Qinglan Liu <qlcactus16@gmail.com>
2019-09-11 20:32:51 +0800

    Update FAD_EFA.R

--- FAD_EFA.R
+++ FAD_EFA.R
@@ -33,7 +33,9 @@ rm('pkgNeeded') # remove the variable 'pkgNeeded';
 #get the data
 df1 <- read.table("FAD_1.csv",header = TRUE, sep = ",")    #n=8248                  
 df2 <- read.table("FAD_2.csv",header = TRUE, sep = ",")    #n=1333,excluded 141,check item, age < 18,not native,not complete,
-df3 <- read.table("FAD_3.csv",header = TRUE, sep = ",")    #n=142 (collected from OSF),exluded 8 cases, time < 60 sec
+df3 <- read.table("FAD_3.csv",header = TRUE, sep = ",")    #n=419 (collected from OSF),exluded 8 cases, time < 60 sec
+df4 <- read.table ("FAD_F.csv",header = TRUE, sep = ",")   #n=711, data of French FAD
+df5 <- read.table ("FAD_J.csv",header = TRUE, sep = ",")   #n=3000, data of Japanese FAD
 #recode df1
 df1 <- df1 %>%                                                                # recode gender_gene,gender_self_report
   mutate_at(c("gender_gene","gender_self_report"),
@@ -51,7 +53,7 @@ df1 <- df1 %>%                                                                # 
 #descriptive
 psych::describe(df1$age)  #mean = 26.51, sd = 5.9
 psych::describe(df2$age)  #mean = 23.22, sd = 5.41
-gendertable1 <- with(df1, table(gender_self_report,education_level))
+gendertable1 <- with(df1, table(gender_gene,education_level))
 print(gendertable1) #male = 2449, female = 2192; edul1=14,edul2=38,edul3=51,edul4=95,edul5=421,edul6=2245,edul7=705,edul8=109
 gendertable2 <- with(df2, table(gender,edu))             
 print(gendertable2) #male = 548,female = 691;eduL1=105,eduL2=80,eduL3=27,eduL4=45,eduL5=354,eduL6=101,eduL7=27
@@ -70,10 +72,10 @@ df1$FW <- rowSums(df1[,FWNames],na.rm = F)/length(FWNames)
 df2$FW <- rowSums(df2[,FWNames],na.rm = F)/length(FWNames)
 #difference between gender
 FADNames <- c("FD","SD","UP","FW")
-t.test(df1$FD ~ df1$gender_self_report) #m=1.34,f=1.57,[-0.26, -0.18],p < 2.2e-16
-t.test(df1$SD ~ df1$gender_self_report) #m=2.17,f=2.18,[-0.04,0.02],p = 0.5394
-t.test(df1$UP ~ df1$gender_self_report) #m=2.18,f=2.19,[-0.04,0.02], p = 0.5588
-t.test(df1$FW ~ df1$gender_self_report) #m=2.20,f=2.24,[-0.07,-0.01],p = 0.01804
+t.test(df1$FD ~ df1$gender_gene)        #m=1.34,f=1.56,[-0.27, -0.18],p < 2.2e-16
+t.test(df1$SD ~ df1$gender_gene)        #m=2.16,f=2.17,[-0.04,0.02],p = 0.62
+t.test(df1$UP ~ df1$gender_gene)        #m=2.18,f=2.19,[-0.05,0.02], p = 0.4812
+t.test(df1$FW ~ df1$gender_gene)        #m=2.21,f=2.24,[-0.06,0.00],p = 0.0511
 t.test(df2$FD ~ df2$gender)             #m=2.35,f=2.38,[-0.11,0.04], p = 0.3652
 t.test(df2$SD ~ df2$gender)             #m=3.06,f=3.09,[-0.09,0.03], p = 0.2987
 t.test(df2$UP ~ df2$gender)             #m=3.06,f=3.07,[-0.07,0.06], p = 0.8724
@@ -81,19 +83,30 @@ t.test(df2$FW ~ df2$gender)             #m=3.27,f=3.38,[-0.18,-0.05], p = 0.0006
 #EFA for df1
 df1.fad <- subset(df1, select = c(FD1:UP27))
 df1.fadcor <- cor(df1.fad,use = "pairwise.complete.obs")   #pairewise deletion for missing value
-psych::fa(df1.fadcor, nfactors = 4, n.obs = 27, rotate = "oblimin", fm = "ml")   #method: oblimin rotation and maximun likelihood extraction
+psych::fa(df1.fadcor, nfactors = 4, n.obs = 8248, rotate = "oblimin", fm = "ml")   #method: oblimin rotation and maximun likelihood extraction
 
 
 #EFA for df2
 df2.fad <- subset(df2, select = c(FD1:FW21,SD22:UP27))
 df2.fadcor <- cor(df2.fad,use = "complete.obs")   #listwise for missing value
-psych::fa(df2.fadcor, nfactors = 4, n.obs = 27, rotate = "oblimin", fm = "ml")   #method: oblimin rotation and maximun likelihood extraction
+psych::fa(df2.fadcor, nfactors = 4, n.obs = 1333, rotate = "oblimin", fm = "ml")   #method: oblimin rotation and maximun likelihood extraction
 
 
 #EFA for df3
 df3.fad <- subset(df3, select = c(FD1:UP27))
 df3.fadcor <- cor(df3.fad,use = "complete.obs")   #listwise for missing value
-psych::fa(df3.fadcor, nfactors = 4, n.obs = 27, rotate = "oblimin", fm = "ml")   #method: oblimin rotation and maximun likelihood extraction
+psych::fa(df3.fadcor, nfactors = 4, n.obs = 419, rotate = "oblimin", fm = "ml")   #method: oblimin rotation and maximun likelihood extraction
+
+
+#EFA for df4
+df4.fad <- subset(df4,select = c(FD1:UP27))
+df4.factor <- cor(df4.fad, use = "complete.obs")  #listwise for missing value
+psych::fa(df4.factor, nfactors = 4, n.obs = 711, rotate = "oblimin", fm = "ml")  #method: oblimin rotation and maximun likelihood extraction
+
+#EFA for df5
+df5.fad <- subset(df5,select = c(FD1:UP27))
+df5.factor <- cor(df5.fad, use = "complete.obs")  #listwise for missing value
+psych::fa(df5.factor, nfactors = 4, n.obs = 3000, rotate = "oblimin", fm = "ml")  #method: oblimin rotation and maximun likelihood extraction
 
 
 
